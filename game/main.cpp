a841fc4bd1802d87165068deb205ccad6a333104
Wolchw <kamcio0610@gmail.com>
2023-03-15 18:28:31 +0100

    Included jumping and collisions Created basic ground without collision

--- game/main.cpp
+++ game/main.cpp
@@ -18,22 +18,3 @@ int main()
 
 
 }
-/*
-		sf::RectangleShape rs(sf::Vector2f(100, 100));
-		rs.setPosition(400, 400);
-		window.clear(sf::Color::Black);
-		window.draw(rs);
-		window.display();
-		while (window.pollEvent(ev))
-		{
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-			{
-				rs.move(-10.f,0);
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-			{
-				rs.move(10.f, 0);
-			}
-			if (e.type == sf::Event::Closed)
-				window.close();
-		}*/
